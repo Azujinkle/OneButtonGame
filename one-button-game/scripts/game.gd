1a019b454e60c00ba9c800c0502154bc16d2da26
Jonny Kwan <jon.s.kwan@gmail.com>
2026-06-17 22:24:26 -0700

    Sleeping controls, energy system

---
+++ one-button-game/scripts/game.gd
@@ -0,0 +1,46 @@
+extends Node2D
+
+var energy: int
+var is_resting: bool
+var is_rem: bool
+
+const STARTING_ENERGY = 1800 # energy at the start of levels
+const AWAKE_ENERGY = -2 # energy lost per frame
+const REST_ENERGY = 3 # energy gained per frame while resting
+const REM_THRESHOLD = 600 # energy needed to awaken if all energy is depleted
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	energy = STARTING_ENERGY
+	is_resting = false
+	is_rem = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if is_rem:
+		close_eyes()
+		if energy > REM_THRESHOLD:
+			is_rem = false
+	else:
+		if Input.is_action_pressed("close"): # default controls
+			close_eyes()
+		else:
+			open_eyes()
+		# TODO: accessibility controls
+	
+	energy += AWAKE_ENERGY
+	if is_resting:
+		energy += REST_ENERGY
+	if energy < 0:
+		is_rem = true
+	$HUD.update_energy(energy, is_resting, is_rem)
+
+# Close the player's eyes, fading out their sights before they recover energy.
+func close_eyes() -> void:
+	await $HUD.close_eyes()
+	is_resting = true
+
+# Open the player's eyes, resuming energy depletion as they regain their sight.
+func open_eyes() -> void:
+	$HUD.open_eyes()
+	is_resting = false
