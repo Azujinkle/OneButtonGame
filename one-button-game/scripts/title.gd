26e1462ca6f432f797c58ea2b36d1e807184ae78
Jeri_H <82069470+JeriH26@users.noreply.github.com>
2026-06-29 11:46:50 -0700

    Add the title background and logo

--- one-button-game/scripts/title.gd
+++ one-button-game/scripts/title.gd
@@ -1,13 +1,30 @@
 extends Control
 
+const TITLE_HOLD_SECONDS := 2.0
+const TITLE_FADE_SECONDS := 0.75
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	_play_title_intro()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _play_title_intro() -> void:
+	$Menu.visible = false
+	$MainTitleSplash.visible = true
+	$MainTitleSplash.modulate.a = 1.0
+
+	await get_tree().create_timer(TITLE_HOLD_SECONDS).timeout
+
+	$Menu.visible = true
+	var tween := create_tween()
+	tween.tween_property($MainTitleSplash, "modulate:a", 0.0, TITLE_FADE_SECONDS)
+	await tween.finished
+	$MainTitleSplash.visible = false
 
 
 func _on_start_pressed() -> void:
